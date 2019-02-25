fab6b987871122f9eed067f031573c50462b96d5
hendisantika <hendisantika@yahoo.co.id>
2019-02-26 06:46:25 +0700

    #4 - Add Person Form Model

---
+++ src/main/java/com/hendisantika/springbootdropdownexample2/model/PersonForm.java
@@ -0,0 +1,19 @@
+package com.hendisantika.springbootdropdownexample2.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-dropdown-example2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-26
+ * Time: 06:45
+ */
+@Data
+public class PersonForm {
+    private String fullName;
+
+    private Long countryId;
+}
