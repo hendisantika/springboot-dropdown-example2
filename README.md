712e720145dfdfdd2101871ab2b75a4a5ded369c
hendisantika <hendisantika@yahoo.co.id>
2019-02-26 06:43:00 +0700

    #1 - Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# springboot-dropdown-example2
---
+++ src/main/java/com/hendisantika/springbootdropdownexample2/SpringbootDropdownExample2Application.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootdropdownexample2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDropdownExample2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDropdownExample2Application.class, args);
+    }
+
+}
