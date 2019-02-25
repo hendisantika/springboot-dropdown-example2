f238ba6e972a8f71faf50650ffbe3c493bf50217
hendisantika <hendisantika@yahoo.co.id>
2019-02-26 06:45:39 +0700

    #3 - Add Country Model

---
+++ src/main/java/com/hendisantika/springbootdropdownexample2/model/Country.java
@@ -0,0 +1,20 @@
+package com.hendisantika.springbootdropdownexample2.model;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : springboot-dropdown-example2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 2019-02-26
+ * Time: 06:44
+ */
+
+@Data
+public class Country {
+    private Long countryId;
+    private String countryCode;
+    private String countryName;
+}
