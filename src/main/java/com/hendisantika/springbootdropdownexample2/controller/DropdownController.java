a8edc56f99628ec81fe2ee942a94578dbe0b67ea
hendisantika <hendisantika@yahoo.co.id>
2019-02-26 07:07:39 +0700

    #8 - Modify templates/selectOptionExample1.html file & Dropdrown Controller

--- src/main/java/com/hendisantika/springbootdropdownexample2/controller/DropdownController.java
+++ src/main/java/com/hendisantika/springbootdropdownexample2/controller/DropdownController.java
@@ -3,10 +3,13 @@ package com.hendisantika.springbootdropdownexample2.controller;
 import com.hendisantika.springbootdropdownexample2.model.Country;
 import com.hendisantika.springbootdropdownexample2.model.PersonForm;
 import com.hendisantika.springbootdropdownexample2.repository.CountryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -22,7 +25,14 @@ import java.util.List;
 @Controller
 public class DropdownController {
 
+    @Autowired
     private CountryRepository countryRepository;
+
+    @GetMapping
+    @ResponseBody
+    public String index() {
+        return "Spring boot Dropdown Example2! " + new Date();
+    }
 
     @GetMapping("/selectOptionExample1")
     public String selectOptionExample1Page(Model model) {
